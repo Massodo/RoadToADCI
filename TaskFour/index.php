c28caffae6c9127d5671a18cca7a1dffca3ab57c
Massodo <pinkiepie1950@gmail.com>
2017-09-28 22:04:46 +0700

    commit

---
+++ TaskFour/index.php
@@ -0,0 +1,43 @@
+<?php
+function generateId($count) {
+	
+	if($count > 0){		
+		$userArray = array();
+	
+		for($i = 0; $i < $count; $i++){
+			$userArray[] = $i + 1;
+		}
+		return $userArray;
+	}
+	return false;
+}
+function selectWinner($userIdArray , $count){	
+	if($count > 0){
+		if(count($userIdArray) > 0){
+			
+			$resultArray = array();
+			$end = true;
+			
+			while($end){
+				$number = mt_rand(0 , count($userIdArray) - 1);
+				if(array_search($userIdArray[$number] , $resultArray) === false){
+					$resultArray[] = $userIdArray[$number];
+				}
+				if(count($resultArray) == $count){
+					$end = false;
+				}
+			}
+			return $resultArray;
+		}
+	}	
+									
+}
+function test(){	
+	print_r(selectWinner(generateId(10) , 9));	
+}
+<<<<<<< HEAD
+test();
+=======
+
+test();
+>>>>>>> aae26d3f049ca4e0c636e139a15110fba1a59659
